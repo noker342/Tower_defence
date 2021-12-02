da13f76e56c07f7df0d26f59c371bfeec4fe21ca
noker342 <71784957+noker342@users.noreply.github.com>
2021-12-02 20:00:53 +0300

    almost

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 width, height = 1920, 1080
@@ -12,27 +13,52 @@ class Scorers_Tower:
         self.center = x, y
         self.x, self.y = x - self.width // 2, y - self.height // 2
         self.radius = 200
-        self.masiv = []
+        self.boomRadius = 100
         self.tower = pygame.image.load('tower_1_lvl.jpeg')
         self.tower_rect = self.tower.get_rect(topleft=(x, y))
 
-    def Draw(self):
+    def draw(self):
         screen.blit(self.tower, self.tower_rect)
 
-    def CheckRange(self, npc):
-        xo = self.center[0] - npc[0]
-        yo = self.center[1] - npc[1]
-        self.theorem = xo ** 2 + yo ** 2
-        if self.theorem <= self.radius:
-            self.masiv.append(self.theorem)
-            print(True)
+    def checkRange(self, npc):
+        npcInRange = []
+        for i in range(len(npc)):
+            xo = self.center[0] - npc[i].x
+            yo = self.center[1] - npc[i].y
+            distance = (xo ** 2 + yo ** 2) ** 0.5
+            if distance <= self.radius:
+                npcInRange.append((npc[i], distance))
+        return npcInRange
 
-    def shot(self):
-        if self.CheckRange:
-            min_range = min(self.masiv)
+    def chooseTarget(self, npc):
+        npcInRange = self.checkRange(npc)
+        if npcInRange:
+            for i in range(len(npcInRange)):
+                minimum = i
+                for j in range(i + 1, len(npcInRange)):
+                    if npcInRange[j][1] < npcInRange[minimum][1]:
+                        minimum = j
+                    npcInRange[minimum], npcInRange[i] = npcInRange[i], npcInRange[minimum]
+        return npcInRange[0][0]
+
+    def shot(self, npc, npc_array):
+        for i in range(len(npc_array)):
+        xo = npc.x - npc_array[i].x
+        yo = npc.y - self.boomRadius
+        boom = (xo ** 2 + yo ** 2) ** 0.5
+        if boom <= self.boomRadius:
+            health -= 100
+
+class Npc:
+    def __init__(self, width, height):
+        self.x = random.randint(0, width)
+        self.y = random.randint(0, height)
 
 
-scorers = Scorers_Tower(200, 200)
+scorers = Scorers_Tower(1920 // 2, 1080 // 2)
+npc = []
+for i in range(100):
+    npc.append(Npc(width, height))
 
 while running:
     for event in pygame.event.get():
@@ -42,8 +68,8 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
     screen.fill((255, 255, 255))
-    scorers.Draw()
-    scorers.CheckRange((210, 210))
-    scorers.CheckRange((324, 210))
-    scorers.shot()
+    scorers.draw()
+    scorers.checkRange(npc)
+    scorers.chooseTarget(npc)
+    scorers.shot(scorers.chooseTarget(npc), npc)
     pygame.display.flip()
